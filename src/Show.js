aa52ff022ecc43e170968be1a60bc3c03b5ca7eb
Palloma Gualter <pallomagualter@gmail.com>
2020-11-17 16:29:07 -0300

    send age

--- src/Show.js
+++ src/Show.js
@@ -1,45 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function Show() {
   return (
     <View style={styles.container}>
-      <TextInput style={styles.textInput}></TextInput>
-      <TextInput style={styles.textInput}></TextInput>
-    </View>
+      <TextInput 
+        style={styles.input}
+        placeholder="Type your name"
+      />
 
+      <TextInput 
+        style={styles.input}
+        placeholder="Type your age"
+      />
+      
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>SEND</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#000',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    text: {
-        color: '#fff',
-    },
-    textInput: {
-      width: 300,
-      height: 50,
-      backgroundColor: '#fff',
-      borderRadius: 10, 
-      textAlign: 'center',
-      marginTop: 5,
-    },
-    button: {
-      height: 50,
-      backgroundColor: '#069',
-      alignSelf: 'stretch',
-      paddingHorizontal: 20,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    buttonText: {
-      color: '#fff',
-      fontWeight: 'bold',
-      fontSize: 20,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#121212',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  input: {
+    height: 45,
+    backgroundColor: '#fff',
+    alignSelf: 'stretch',
+    borderColor: '#EEE',
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 20,
+    marginBottom: 10,
+  },
+  button: {
+    height: 45,
+    backgroundColor: '#8257E6',
+    alignSelf: 'stretch',
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
   });
